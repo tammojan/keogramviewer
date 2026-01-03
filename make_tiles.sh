729aa00ff5e7a0890718c5b8a2ec75661f52a89f
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2026-01-03 12:48:16 +0100

    Full-res version 2025

--- make_tiles.sh
+++ make_tiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-input=../2025_yearly.png #yearlong_keogram.png
-xpix=1440
-ypix=18250 #329400
+input=../2025_yearly_fullres.png
+xpix=$(identify -format '%w' "$input")
+ypix=$(identify -format '%h' "$input")
 
-for level in {0..8}; do
+for level in {0..9}; do
     mkdir -p tiles/$level
     scale=$((2**$level))
     convert $input -scale 1440x$((${ypix}/$scale))\! -crop 1440x1024 +repage tiles/${level}/%d.jpg
